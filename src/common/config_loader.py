20a440643b6d73af8bcb64bded6049f24c65e834
Márcio Lima <marciocad@gmail.com>
2026-04-05 12:47:04 -0300

    refactor: em razão de mudança de nome de arquivo

--- src/common/config_loader.py
+++ src/common/config_loader.py
@@ -3,9 +3,10 @@ from pathlib import Path
 import yaml
 
 ROOT_DIR = Path(__file__).resolve().parents[2]
+DEFAULT_GLOBAL_CONFIG_PATH = "configs/pipeline_global_config.yaml"
 
 
-def load_config(config_path: str = "configs/pipeline_config.yaml") -> dict:
+def load_config(config_path: str = DEFAULT_GLOBAL_CONFIG_PATH) -> dict:
     full_path = ROOT_DIR / config_path
     with open(full_path, "r", encoding="utf-8") as f:
         return yaml.safe_load(f)
